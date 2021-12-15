420740ba04cca212ba05af4bf5fdc56f0a55852e
J Lee <jay.j.lee.1@gmail.com>
2021-12-15 16:16:03 -0500

    commit tree 355100d4ed6d1c44e0efd44c978c49a03036862a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (drivers, string) {
+    let answers = drivers.filter(el => el.toLowerCase() === string.toLowerCase())
+    return answers
+}
+
+function fuzzyMatch (drivers, string) {
+    let starts = drivers.filter(startname => startname.startsWith(string) === true)
+    return starts
+}
+
+function matchName (drivers, string) {
+    let names = drivers.filter(el => el.name === string)
+    return names
+}
